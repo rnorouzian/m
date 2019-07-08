25df239d39d11e89fdadb321d7b65cb8beabe8f3
Reza Norouzian <rnorouzian@gmail.com>
2019-07-08 15:36:17 -0500

    Update m.r

--- m.r
+++ m.r
@@ -1,3 +1,26 @@
+Break = "\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n"
+
+notice = "  A suite of R functions for Modern Meta-Analysis in Second Language Acquisition Research.
+    Copyright (C) 2019-present  Reza Norouzian, rnorouzian@gmail.com
+
+    This set of programs is free software: you can redistribute it under the 
+    terms of the GNU General Public License as published by the Free 
+    Software Foundation, either version 3 of the License, or any later 
+    version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>."
+
+message(Break, notice, Break)
+
+
+#==================================================================================================================
+
 
 autoreg <- function(steps, r){
   
