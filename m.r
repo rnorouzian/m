d29cbe46a6b55a07b50fbba6f3ac68a5d756e8e3
Reza Norouzian <rnorouzian@gmail.com>
2019-11-06 22:50:38 -0600

    Update m.r

--- m.r
+++ m.r
@@ -5083,31 +5083,11 @@ meta.bayes <- function(data = NULL, by, option = 1, r = .5, mu.prior = c("mean" 
                 
 #==============================================================================================================================================   
                 
-find.norms <- function(low, high, cover = .99, digits = 6){
-  
-f <- Vectorize(function(low, high, cover, digits){
-  
-options(warn = -1)
-p1 <- (1 - cover) / 2
-p2 <- 1 - p1
-q <- c(low, high)
-alpha <- c(p1, p2)
-is.df <- function(a, b, sig = 4) (round(a, sig) != round(b, sig))
-if (p1 <= 0 || p2 >= 1 || q[1] >= q[2] || p1 >= p2) {
-stop("Change 'low' and/or 'high' values.", call. = FALSE)
-} else {
-beta <- qnorm(alpha)
-parm <- solve(cbind(1, beta), q)
-q <- qnorm(c(p1, p2), parm[[1]], parm[[2]])
-}
-if(is.df(low, q[[1]]) || is.df(high, q[[2]])) {
-stop("Change 'low' and/or 'high' values.", call. = FALSE)
-} else {
-return(c(mean = round(parm[[1]], digits = digits), sd = round(parm[[2]], digits = digits)))
-   }
-})
-data.frame(t(f(low = low, high = high, cover = cover, digits = digits)))
-}              
+ find.norms <- function(low, high, cover = .99, digits = 6){
+   
+   f <- Vectorize(mu.norm)
+   data.frame(t(f(low = low, high = high, cover = cover, digits = digits)))
+ }              
                 
 #====================================================================================================
                 
