9463ba9bc05e622704d0c8dda6fde5a595887eaf
Reza Norouzian <rnorouzian@gmail.com>
2019-11-18 20:07:39 -0600

    Update m.r

--- m.r
+++ m.r
@@ -5641,22 +5641,16 @@ exam.code <- function(data, exclude = NULL, rule = 1, lwd = 4, lend = 2, cat.lev
 #================================================================================================================================================================
                                       
 robu.weight <- function(...){
-
+  
   if(!all(sapply(list(...), inherits, "robu"))) stop("Non-robust variance model(s) detected.", call. = FALSE)
   
   m <- list(...)
   n <- substitute(...())
   
-f <- function(fit){
-  
-  d <- fit$data  
-  tau.sq <- fit$mod_info$tau.sq[[1]]
-  k <- table(d$id)[d$id]
-  Vbar <- tapply(d$SD^2, d$id, mean)[d$id]
-  as.numeric(1/(k * (Vbar + tau.sq)))
-}
-
-setNames(lapply(m, f), n)
+  f <- function(fit){
+    fit$data.full$r.weights
+  }
+  setNames(lapply(m, f), n)
 }                                     
 
 #================================================================================================================================================================
