ecb20815d56533aa6194508e9ab8f2ed18dbcf85
Reza Norouzian <rnorouzian@gmail.com>
2019-12-16 00:05:47 -0600

    Update qs.r

--- qs.r
+++ qs.r
@@ -1,6 +1,6 @@
 
 # Suppose a human skill can be measured by asking 15 questions that are each worth 2 points, 
-# what can be a likely distribution of overall scores of 5000 randomly selected test-takers?
+# what can be a likely distribution of overall scores (i.e., sum of 15 questions) of 5000 randomly selected test-takers?
 
 # USE R to find an answer:
 add.norm <- function(n.test.taker = 5e3, n.question = 15, pt.worth = 2){
@@ -21,4 +21,5 @@ mult.norm <- function(n.subject = 5e3, n.subskill = 5, max.small.growth = .1){
   growth <- replicate(n.subject, prod(1 + runif(n.subskill, 0, max.small.growth)))
   
   plot(density(growth), main = "Growth Distribution", xlab = "Growth Rate")
+  mean(growth)
 }
